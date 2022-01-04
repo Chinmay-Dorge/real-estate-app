0e4b141d06a45e7727aa5c44a8ab06a5287d7836
chinmay <lcs2020022@iiitl.ac.in>
2022-01-04 18:35:15 +0530

    Added search

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,13 @@
+import {useEffect , useState} from 'react'
+import {Box, Flex , Icon ,Button , Select, Text, Input, Spinner} from '@chakra-ui/react'
+import {useRouter} from 'next/router'
+import { MdCancel} from 'react-icons/md'
+import Image from 'next/image'
+
+const SearchFilters = () =>{
+    return (
+        <div>SearchFilters</div>
+    )
+}
+
+export default SearchFilters;
